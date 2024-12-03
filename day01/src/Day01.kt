0048a8d245b22ef6ca2f2ac14db3affbfd979f61
Simon Wilson <ksattic@gmail.com>
2024-12-02 20:01:54 -0700

    Day 2 solution

--- day01/src/Day01.kt
+++ day01/src/Day01.kt
@@ -1,2 +1,40 @@
-class Day01 {
+import java.io.File
+import kotlin.math.absoluteValue
+
+class Day01(file: String) {
+    private val listA = mutableListOf<Int>()
+    private val listB = mutableListOf<Int>()
+
+    init {
+        File(file).forEachLine {
+            val row = it.split("   ")
+            listA.add(row[0].toInt())
+            listB.add(row[1].toInt())
+        }
+
+        listA.sort()
+        listB.sort()
+    }
+
+    fun part1(): Int {
+        var sum = 0
+        listA.forEachIndexed { index, a ->
+            sum += (a - listB[index]).absoluteValue
+        }
+        return sum
+    }
+
+    fun part2(): Int {
+        var similarity = 0
+        listA.forEach { a ->
+            similarity += a * listB.count { it == a }
+        }
+        return similarity
+    }
+}
+
+fun main() {
+    val aoc = Day01("day01/input.txt")
+    println(aoc.part1())
+    println(aoc.part2())
 }
--- day02/src/Day02.kt
+++ day02/src/Day02.kt
@@ -1,2 +1,62 @@
-class Day02 {
+import java.io.File
+import kotlin.math.absoluteValue
+
+class Day02(private val file: String) {
+    private fun isReportValid(levels: List<Int>): Boolean {
+        val deltas = levels.zipWithNext { a, b -> b - a }
+        var isSafe = true
+        val isPositive = deltas[0] > 0
+        deltas.forEach {
+            if ((it > 0 != isPositive) || (it.absoluteValue > 3) || (it.absoluteValue == 0)) {
+                isSafe = false
+            }
+        }
+        return isSafe
+    }
+
+    private fun isModifiedReportValid(levels: List<Int>): Boolean {
+        if (isReportValid(levels)) {
+            return true
+        }
+
+        // Remove one element at each index to generate a new levels list
+        for (i in levels.indices) {
+            val newLevels = levels.toMutableList().apply {
+                removeAt(i)
+            }
+            if (isReportValid(newLevels)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun part1(): Int {
+        var count = 0
+        File(file).forEachLine { report ->
+            val levels = report.split(" ").map { it.toInt() }
+            if (isReportValid(levels)) {
+                count++
+            }
+        }
+        return count
+    }
+
+    fun part2(): Int {
+        var count = 0
+        File(file).forEachLine { report ->
+            val levels = report.split(" ").map { it.toInt() }
+            if (isModifiedReportValid(levels)) {
+                count++
+            }
+        }
+        return count
+    }
+}
+
+fun main() {
+    val aoc = Day02("day02/input.txt")
+    println(aoc.part1())
+    println(aoc.part2())
 }
