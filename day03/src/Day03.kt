1a295322215a3d721f899d550ca1aa11a488bdd5
Simon Wilson <ksattic@gmail.com>
2024-12-03 20:16:16 -0700

    Day 3 part 2 solution

--- day03/src/Day03.kt
+++ day03/src/Day03.kt
@@ -15,9 +15,44 @@ class Day03(private val file: String) {
         }
         return sum
     }
+
+    fun part2(): Int {
+        var sum = 0
+        var enable = true
+        val regex = "\\d+,\\d+".toRegex()
+        File(file).forEachLine { line ->
+            var i = 0
+            while (i < line.length) {
+                if ((i < line.length - 4) && (line.substring(i, i + 4) == "mul(")) {
+                    i += 4
+                    if (enable) {
+                        // Now scan for a valid xxx,yyy pair inside the parens
+                        val j = line.indexOf(')', i)
+                        if (j < line.length) {
+                            val pair = line.substring(i, j)
+                            if (regex.matches(pair)) {
+                                val numbers = pair.split(",").map { it.toInt() }
+                                sum += numbers[0] * numbers[1]
+                            }
+                        }
+                    }
+                } else if ((i < line.length - 4) && (line.substring(i, i + 4) == "do()")) {
+                    enable = true
+                    i += 4
+                } else if ((i < line.length - 7) && (line.substring(i, i + 7) == "don't()")) {
+                    enable = false
+                    i += 7
+                } else {
+                    i++
+                }
+            }
+        }
+        return sum
+    }
 }
 
 fun main() {
     val aoc = Day03("day03/input.txt")
     println(aoc.part1())
+    println(aoc.part2())
 }
